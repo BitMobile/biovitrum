0429a80cf38b0e3be2c202f2db0cfab24fc7af77
Dmitrii Kuznechenkov <DPKuznechenkov@1cbit.ru>
2016-05-17 16:33:21 +0300

    Заглушки для Бд и небольшой рефакторинг.

--- SuperService/Components/OrderListScreen.cs
+++ SuperService/Components/OrderListScreen.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using BitMobile.ClientModel3;
@@ -11,57 +11,27 @@ namespace Test
     {
         private VerticalLayout _vlSlideVerticalLayout;
         private SwipeVerticalLayout _svlOrderList;
+        private ArrayList _ordersList;
 
         public override void OnLoading()
         {
             _vlSlideVerticalLayout = (VerticalLayout)GetControl("SlideVerticalLayout", true);
             _svlOrderList = (SwipeVerticalLayout)GetControl("OrderList", true);
-            GetOrdersList();
+            _ordersList = GetOrdersFromDb();
+            FillingOrderList();
         }
 
-        private void GetOrdersList()
+        private void FillingOrderList()
         {
-            HorizontalLayout _hlTemp;
-            VerticalLayout _vlLeftSideTemp;
-            VerticalLayout _vlPriority;
-            VerticalLayout _vlRightSideTemp;
 
-            _vlLeftSideTemp = FillLeftSideTemp();
+            if (_ordersList == null)
+                return;
 
-            _vlPriority = new VerticalLayout();
-            _vlPriority.AddChild(new Image());
+            foreach(var item in _ordersList)
+            {
+                //Filling Order List
+            }
 
-
-            _vlRightSideTemp = new VerticalLayout();
-            FillRIghtSideTemp(_vlRightSideTemp);
-
-            _hlTemp = new HorizontalLayout();
-            FillHorizontalLayout(_hlTemp, _vlLeftSideTemp, _vlPriority, _vlRightSideTemp);
-
-            _svlOrderList.AddChild(_hlTemp);
-        }
-
-        private void FillHorizontalLayout(HorizontalLayout _hlTemp, VerticalLayout _vlLeftSideTemp, VerticalLayout _vlPriority, VerticalLayout _vlRightSideTemp)
-        {
-            _hlTemp.AddChild(_vlLeftSideTemp);
-            _hlTemp.AddChild(_vlPriority);
-            _hlTemp.AddChild(_vlRightSideTemp);
-            _hlTemp.OnClick += GoToOrderScreen_OnClick;
-        }
-
-        private static void FillRIghtSideTemp(VerticalLayout _vlRightSideTemp)
-        {
-            _vlRightSideTemp.AddChild(new TextView("Name of company"));
-            _vlRightSideTemp.AddChild(new TextView("Adress"));
-            _vlRightSideTemp.AddChild(new TextView("Type"));
-        }
-
-        private static VerticalLayout FillLeftSideTemp()
-        {
-            VerticalLayout _vlLeftSideTemp = new VerticalLayout();
-            _vlLeftSideTemp.AddChild(new TextView("Time to start"));
-            _vlLeftSideTemp.AddChild(new TextView("Is start?"));
-            return _vlLeftSideTemp;
         }
 
         internal void GoToMap_OnClick(object sender, EventArgs e)
@@ -74,5 +44,10 @@ namespace Test
             BusinessProcess.DoAction("ViewOrder");
         }
 
+        private ArrayList GetOrdersFromDb()
+        {
+            //Получение данных из БД.
+            return new ArrayList();
+        }
     }
 }
---
+++ SuperService/Entities/Document/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BitMobile.DbEngine;
+
+namespace Test.Document
+{
+    public class Event : DbEntity
+    {
+        public DateTime StartDatePlane { get; set; }
+        public DateTime ActualStartDate { get; set; }
+        public string Importance { get; set; }
+        public string ClientDescription { get; set; }
+        public string ClientAdress { get; set; }
+        public string TypeDeparture { get; set; }
+    }
+}
