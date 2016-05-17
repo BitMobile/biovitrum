a4f7a932f199e02861b682122bee3b6487006149
Ilya Kuznetsov <xill.kuz@gmail.com>
2016-05-17 16:02:42 +0300

    Заглушки для бд
    А так же сущности для будущей бд

--- SuperService/Controllers/OrderScreen.cs
+++ SuperService/Controllers/OrderScreen.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using BitMobile.ClientModel3.UI;
+using Test.Catalog;
+using Test.Document;
 
 namespace Test
 {
@@ -7,9 +9,10 @@ namespace Test
     {
         public override void OnLoading()
         {
-
         }
 
+
+        // TODO: Сделать это работать
         internal void BackButton_OnClick(object sender, EventArgs eventArgs)
         {
         }
@@ -24,6 +27,55 @@ namespace Test
 
         internal void CancelButton_OnClick(object sender, EventArgs eventArgs)
         {
+        }
+
+
+        // TODO: Работа с базой данных
+        private Event GetCurrentOrder()
+        {
+            return new Event
+            {
+                Comment = "Плановое подключение к третьей сети, при монтаже быть аккуратным с текущей коммуникацией.",
+                StartDatePlan = new DateTime(2017, 05, 17, 10, 00, 00)
+            };
+        }
+
+        private TypesDepartures GetDepartureType()
+        {
+            return new TypesDepartures()
+            {
+                Description = "Монтаж"
+            };
+        }
+
+        private int GetTaskNumber()
+        {
+            return 9;
+        }
+
+        private int GetTaskNumberDone()
+        {
+            return 4;
+        }
+
+        private decimal GetCertificateOfCompletion()
+        {
+            return new decimal(1440.00);
+        }
+
+        private int GetCheckListNumber()
+        {
+            return 9;
+        }
+
+        private int GetCheckListDone()
+        {
+            return 5;
+        }
+
+        private bool GetCheckListRequired()
+        {
+            return true;
         }
     }
 }
