de19eca41a52b19803546a819c560beae546ff77
Ilya Kuznetsov <xill.kuz@gmail.com>
2016-05-17 13:00:16 +0300

    События на кнопках и поддержка локализации

--- SuperService/Controllers/OrderScreen.cs
+++ SuperService/Controllers/OrderScreen.cs
@@ -1,9 +1,29 @@
-﻿using BitMobile.ClientModel3.UI;
+﻿using System;
+using BitMobile.ClientModel3.UI;
 
 namespace Test
 {
     public class OrderScreen : Screen
     {
-        
+        public override void OnLoading()
+        {
+
+        }
+
+        internal void BackButton_OnClick(object sender, EventArgs eventArgs)
+        {
+        }
+
+        internal void ClientInfoButton_OnClick(object sender, EventArgs eventArgs)
+        {
+        }
+
+        internal void StartButton_OnClick(object sender, EventArgs eventArgs)
+        {
+        }
+
+        internal void CancelButton_OnClick(object sender, EventArgs eventArgs)
+        {
+        }
     }
 }
