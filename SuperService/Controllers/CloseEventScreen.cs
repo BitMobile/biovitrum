b1d8cc954f27893246607d60058cc930a6986f21
Ilya Kuznetsov <xill.kuz@gmail.com>
2016-05-24 14:45:02 +0300

    Верстка и логика готовы, жду иконок

--- SuperService/Controllers/CloseEventScreen.cs
+++ SuperService/Controllers/CloseEventScreen.cs
@@ -5,7 +5,60 @@ namespace Test
 {
     public class CloseEventScreen : Screen
     {
-        internal void FinishButton_OnClick(object sernder, EventArgs eventArgs)
+        private bool _problem;
+        private HorizontalLayout _problemButton;
+        private VerticalLayout _problemCommentLayout;
+        private bool _wantToBuy;
+        private HorizontalLayout _wantToBuyButton;
+        private VerticalLayout _wantToBuyCommentLayout;
+
+        public override void OnLoading()
+        {
+            _wantToBuyButton = (HorizontalLayout) GetControl("WantToBuyButton", true);
+            _wantToBuyCommentLayout = (VerticalLayout) GetControl("WantToBuyCommentLayout", true);
+            _problemButton = (HorizontalLayout) GetControl("ProblemButton", true);
+            _problemCommentLayout = (VerticalLayout) GetControl("ProblemCommentLayout", true);
+        }
+
+        internal void WantToBuyButton_OnClick(object sender, EventArgs eventArgs)
+        {
+            if (!_wantToBuy)
+            {
+                UpdateButtonCSS(_wantToBuyButton, _wantToBuyCommentLayout, "BigButtonPressed", "CommentLayout");
+                _wantToBuy = true;
+            }
+            else
+            {
+                UpdateButtonCSS(_wantToBuyButton, _wantToBuyCommentLayout, "BigButton", "NoHeight");
+                _wantToBuy = false;
+            }
+        }
+
+        private void UpdateButtonCSS(HorizontalLayout buttonLayout, VerticalLayout commentLayout, string buttonCSS,
+            string commentCSS)
+        {
+            buttonLayout.CssClass = buttonCSS;
+            buttonLayout.Refresh();
+            commentLayout.CssClass = commentCSS;
+            commentLayout.Refresh();
+        }
+
+
+        internal void ProblemButton_OnClick(object sender, EventArgs eventArgs)
+        {
+            if (!_problem)
+            {
+                UpdateButtonCSS(_problemButton, _problemCommentLayout, "BigButtonPressed", "CommentLayout");
+                _problem = true;
+            }
+            else
+            {
+                UpdateButtonCSS(_problemButton, _problemCommentLayout, "BigButton", "NoHeight");
+                _problem = false;
+            }
+        }
+
+        internal void FinishButton_OnClick(object sender, EventArgs eventArgs)
         {
             // TODO: Закрытие наряда
             BusinessProcess.DoAction("FinishEvent");
