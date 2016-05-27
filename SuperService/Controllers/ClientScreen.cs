e6e0efacabdb341f510dcd2ac9e236fe9095316c
Dmitrii <DPKuznechenkov@1cbit.ru>
2016-05-27 11:26:46 +0300

    Удалены ненужные компоненты из кода.

--- SuperService/Controllers/ClientScreen.cs
+++ SuperService/Controllers/ClientScreen.cs
@@ -7,23 +7,21 @@ namespace Test
 {
     public class ClientScreen : Screen
     {
-        private TopComponent _topComponent;
+        
 
         public override void OnLoading()
         {
-            _topComponent = new TopComponent(this);
-            _topComponent.TopHeadingTextView.Text = Translator.Translate("client");
-            _topComponent.TopRightButtonImage.Source = @"Image\top_ico_edit.png";
+            
         }
 
         internal void Top_LeftButton_OnClick(object sender, EventArgs e)
         {
-            _topComponent.LeftButton_OnClick("ViewEvent");
+            
         }
 
         internal void Top_RightButton_OnClick(object sender, EventArgs e)
         {
-            //_topComponent.RightButton_OnClick();
+            
             DConsole.WriteLine("Some Action");
         }
 
