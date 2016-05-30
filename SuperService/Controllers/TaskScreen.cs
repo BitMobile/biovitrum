f6d66ec115c3702f28a504846ecc50dbf3dc2074
Ilya Kuznetsov <xill.kuz@gmail.com>
2016-05-30 17:59:24 +0300

    Сквошнул изменения DBHelper и подключил заголовочный компонент

--- SuperService/Controllers/TaskScreen.cs
+++ SuperService/Controllers/TaskScreen.cs
@@ -1,14 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using BitMobile.ClientModel3;
 using BitMobile.ClientModel3.UI;
+using Test.Components;
 
 namespace Test
 {
     public class TaskScreen : Screen
     {
-        internal void BackButton_OnClick(object sender, EventArgs eventArgs)
+        private TopInfoComponent _topInfoComponent;
+
+        public override void OnLoading()
         {
-            BusinessProcess.DoAction("BackToTaskList");
+            _topInfoComponent = new TopInfoComponent(this)
+            {
+                HeadingTextView = {Text = Translator.Translate("task")},
+                LeftButtonImage = {Source = ResourceManager.GetImage("topheading_back")},
+                RightButtonImage = {Visible = false},
+                ExtraLayoutVisible = false
+            };
         }
 
         internal void TaskFinishedButton_OnClick(object sender, EventArgs eventArgs)
@@ -23,7 +33,17 @@ namespace Test
 
         internal void TopInfo_Arrow_OnClick(object sender, EventArgs eventArgs)
         {
-            
+            _topInfoComponent.Arrow_OnClick(sender, eventArgs);
+        }
+
+        internal void TopInfo_RightButton_OnClick(object sender, EventArgs eventArgs)
+        {
+            DConsole.WriteLine("HOW? WHY?");
+        }
+
+        internal void TopInfo_LeftButton_OnClick(object sender, EventArgs eventArgs)
+        {
+            BusinessProcess.DoAction("BackToTaskList");
         }
 
         internal object GetTask()
