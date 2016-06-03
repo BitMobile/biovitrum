0a98432aefbb9a15e3953881b39dffbe16b59a30
mvsidorov <MVSidorov@1cbit.ru>
2016-06-03 16:16:18 +0300

    работы по блоку услуг в АВР

---
+++ SuperService/Objects/EventServicesMaterialsLine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+
+    /// <summary>
+    ///     Является проекций строки табличной части услуги и материалы документа наряд
+    /// </summary>
+    /// <remarks>
+    /// </remarks>
+    public class EventServicesMaterialsLine
+    {
+
+        public string ID { get; set; }
+        public int LineNumber { get; set; }
+        public string Ref { get; set; }
+        public string SKU { get; set; }
+        // TODO: покурить с даблом, что бы не нарваться на проблему округления
+        public double Price { get; set; }
+        public double AmountPlan { get;  set; }
+        public double SumPlan { get; set; }
+        public double AmountFact { get; set; }
+        public double SumFact { get; set; }
+
+    }
+}
