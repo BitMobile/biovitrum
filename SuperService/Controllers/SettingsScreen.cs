870dd6ccb69bf845b57d3f53be1d1f848f8292fa
Alexander <a.s.rumyantsev@gmail.com>
2016-06-07 12:41:52 +0300

    Поправил TabBar на всех экранах, почти закончил логику

--- SuperService/Controllers/SettingsScreen.cs
+++ SuperService/Controllers/SettingsScreen.cs
@@ -10,11 +10,35 @@ namespace Test
     public class SettingsScreen : Screen
     {
         private TabBarComponent _tabBarComponent;
+        private TopInfoComponent _topInfoComponent;
+
+        // TODO: Тут будет экран настроек
 
         public override void OnLoading()
         {
-            _tabBarComponent = new TabBarComponent(this);
             DConsole.WriteLine("SettingsScreen init");
+
+            _topInfoComponent = new TopInfoComponent(this)
+            {
+                HeadingTextView = { Text = Translator.Translate("settings") },
+                LeftButtonImage = { Visible = false },
+                RightButtonImage = { Visible = false },
+                ExtraLayoutVisible = false
+            };
+
+            _tabBarComponent = new TabBarComponent(this);
+        }
+
+        internal void TopInfo_LeftButton_OnClick(object sender, EventArgs e)
+        {
+        }
+
+        internal void TopInfo_RightButton_OnClick(object sender, EventArgs e)
+        {
+        }
+
+        internal void TopInfo_Arrow_OnClick(object sender, EventArgs e)
+        {
         }
 
         internal void TabBarFirstTabButton_OnClick(object sender, EventArgs eventArgs)
