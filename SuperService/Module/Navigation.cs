4fcda62dc5f179bc1e6217f959f0acaad2ed9f44
Ilya Kuznetsov <xill.kuz@gmail.com>
2016-06-20 13:57:55 +0300

    Переименовал ScreenInfoStack в ScreenStack

--- SuperService/Module/Navigation.cs
+++ SuperService/Module/Navigation.cs
@@ -8,18 +8,19 @@ namespace Test
     public static class Navigation
     {
         private const string DefaultStyle = @"Style\style.css";
+        private static readonly Stack ScreenInfoStack = new Stack();
         private static readonly Stack ScreenStack = new Stack();
 
         public static ScreenInfo CurrentScreenInfo { get; private set; }
 
         public static void Back(bool reload = false)
         {
-            if (ScreenStack.Count == 0)
+            if (ScreenInfoStack.Count == 0)
             {
                 DConsole.WriteLine("Can't go back when stack is empty");
                 return;
             }
-            var screenInfo = (ScreenInfo) ScreenStack.Pop();
+            var screenInfo = (ScreenInfo) ScreenInfoStack.Pop();
             if (reload)
                 screenInfo.Screen = (Screen) Application.CreateInstance($"Test.{screenInfo.Name}");
             ModalMove(screenInfo);
@@ -27,16 +28,21 @@ namespace Test
 
         public static void MoveTo(string name, string css = null, Dictionary<string, object> args = null)
         {
+            DConsole.WriteLine("Loading screen info");
             var screenInfo = CreateScreenInfoFromName(name, css);
+            DConsole.WriteLine("Really moving somewhere");
             MoveTo(screenInfo, args);
         }
 
         private static ScreenInfo CreateScreenInfoFromName(string name, string css)
         {
+            var screen = (Screen) Application.CreateInstance($"Test.{name}");
+            DConsole.WriteLine($"Created instance of {name}");
+            DConsole.WriteLine($"ORLY? : {screen == null}");
             return new ScreenInfo
             {
                 Name = name,
-                Screen = (Screen) Application.CreateInstance($"Test.{name}"),
+                Screen = screen,
                 Xml = $@"Screen\{name}.xml",
                 Css = css ?? $@"Style\{name}.css"
             };
@@ -44,8 +50,10 @@ namespace Test
 
         public static void MoveTo(ScreenInfo screenInfo, Dictionary<string, object> args = null)
         {
+            DConsole.WriteLine("Is there CSI?");
             if (CurrentScreenInfo != null)
-                ScreenStack.Push(CurrentScreenInfo);
+                ScreenInfoStack.Push(CurrentScreenInfo);
+            DConsole.WriteLine("Ok, now really moving");
             ModalMove(screenInfo, args);
         }
 
@@ -57,7 +65,9 @@ namespace Test
 
         public static void ModalMove(ScreenInfo screenInfo, Dictionary<string, object> args = null)
         {
+            DConsole.WriteLine("Trying to get screen from info");
             var screen = screenInfo.Screen;
+            DConsole.WriteLine("Basic screen interaction");
             screen.SetData(args);
             try
             {
@@ -75,7 +85,9 @@ namespace Test
             {
                 screen.LoadStyleSheet(Application.GetResourceStream(DefaultStyle));
             }
+            DConsole.WriteLine("Now we can show this");
             screen.Show();
+            DConsole.WriteLine("Oh, and don't forget to save new info");
             CurrentScreenInfo = screenInfo;
         }
     }
