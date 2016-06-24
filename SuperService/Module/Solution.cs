dbf01f7884efb9e92c6db8948c17b062bd546200
Ilya Kuznetsov <xill.kuz@gmail.com>
2016-06-24 18:50:49 +0300

    Обновил версию
    Вернул запуск как было

--- SuperService/Module/Solution.cs
+++ SuperService/Module/Solution.cs
@@ -9,10 +9,7 @@ namespace Test
             DConsole.WriteLine("DB init...");
             DBHelper.Init();
             DConsole.WriteLine("Loading first screen");
-            BusinessProcess.GlobalVariables[Parameters.IdCurrentEventId] =
-                "@ref[Document_Event]:c2e83f87-218a-11e6-80e3-005056011152";
-            Navigation.Move("CheckListScreen");
-            //            Navigation.Move("EquipmentScreen");
+            Navigation.Move("AuthScreen");
         }
     }
 }
