169f95dcc50357f0f8a7ba6c11e546e21c6f0a0c
Ilya Kuznetsov <xill.kuz@gmail.com>
2016-06-28 18:44:34 +0300

    Вынес ресурсы локализации и почистил код

--- SuperService/Controllers/ContactScreen.cs
+++ SuperService/Controllers/ContactScreen.cs
@@ -1,19 +1,26 @@
-﻿using BitMobile.ClientModel3.UI;
+﻿using BitMobile.ClientModel3;
+using BitMobile.ClientModel3.UI;
 using Test.Entities.Catalog;
 
 namespace Test
 {
     public class ContactScreen : Screen
     {
-        internal Contacts GetContact()
+        public ContactScreen()
         {
-            return new Contacts()
+            DConsole.WriteLine("Constructor");
+            Variables["contact"] = new Contacts
             {
                 Description = "Сергеев Алексей",
                 Position = "Старший инженер",
                 Tel = "+7 (915) 382-54-62",
                 EMail = "nsergeev@comp.ru"
             };
+        }
+
+        public override void OnLoading()
+        {
+            DConsole.WriteLine("OnLoading");
         }
     }
 }
