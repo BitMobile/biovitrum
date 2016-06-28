30acaae99ef62540e3c98ac4980ca82d37dd1410
Ilya Kuznetsov <xill.kuz@gmail.com>
2016-06-28 18:20:28 +0300

    Набросок логики отображения контакта

---
+++ SuperService/Controllers/ContactScreen.cs
@@ -0,0 +1,19 @@
+﻿using BitMobile.ClientModel3.UI;
+using Test.Entities.Catalog;
+
+namespace Test
+{
+    public class ContactScreen : Screen
+    {
+        internal Contacts GetContact()
+        {
+            return new Contacts()
+            {
+                Description = "Сергеев Алексей",
+                Position = "Старший инженер",
+                Tel = "+7 (915) 382-54-62",
+                EMail = "nsergeev@comp.ru"
+            };
+        }
+    }
+}
