8a22d0303d30eda39ba995bed79935bb724fbcb8
Ilya Kuznetsov <xill.kuz@gmail.com>
2016-06-29 12:56:12 +0300

    Почти закончил логику, осталось прописать переходы

--- SuperService/Controllers/ContactScreen.cs
+++ SuperService/Controllers/ContactScreen.cs
@@ -1,24 +1,43 @@
 ﻿using BitMobile.ClientModel3;
 using BitMobile.ClientModel3.UI;
+using System;
 using Test.Entities.Catalog;
 
 namespace Test
 {
     public class ContactScreen : Screen
     {
+        private Contacts _contact;
+
         public ContactScreen()
         {
-            Variables["contact"] = new Contacts
+            Variables[Parameters.Contact] = new Contacts
             {
                 Description = "Сергеев Алексей",
                 Position = "Старший инженер",
-                Tel = "+7 (915) 382-54-62",
+                Tel = "+7 (921) 859-93-29",
                 EMail = "nsergeev@comp.ru"
             };
         }
 
         public override void OnLoading()
         {
+            _contact = (Contacts)Variables.GetValueOrDefault(Parameters.Contact);
+        }
+
+        internal void CallButton_OnClick(object o, EventArgs e)
+        {
+            Phone.Call(_contact.Tel);
+        }
+
+        internal void SendMessageButton_OnClick(object o, EventArgs e)
+        {
+            Dialog.Message("Мы не можем отправлять сообщения");
+        }
+
+        internal void WriteLetterButton_OnClick(object o, EventArgs e)
+        {
+            Dialog.Message("Мы не можем отправлять EMail");
         }
     }
 }
--- SuperService/Parameters/Parameters.cs
+++ SuperService/Parameters/Parameters.cs
@@ -6,19 +6,17 @@ using System.Threading.Tasks;
 
 namespace Test
 {
-    static class Parameters
+    internal static class Parameters
     {
-
-
         //Идентификаторы передаваемых между экранами параметров
         public const string IdCurrentEventId = "currentEventId";
+
         public const string IdScreenStateId = "screenState";
         public const string IdClientId = "clientId";
         public const string IdIsService = "isService";
         public const string IdBehaviour = "behaviour";
         public const string IdLineId = "lineId";
         public const string IdEquipmentId = "idEquipmentId";
-
-
+        public const string Contact = "contact";
     }
 }
