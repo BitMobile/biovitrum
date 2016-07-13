e6d9d147d51bffb148a09be93ac102ead72896a3
Ilya Kuznetsov <xill.kuz@gmail.com>
2016-07-13 15:41:41 +0300

    Экран сделал, экран работает

--- SuperService/Controllers/CancelEventScreen.cs
+++ SuperService/Controllers/CancelEventScreen.cs
@@ -1,8 +1,53 @@
-﻿using BitMobile.ClientModel3.UI;
+﻿using BitMobile.ClientModel3;
+using BitMobile.ClientModel3.UI;
+using BitMobile.DbEngine;
+using System;
+using Test.Components;
+using Test.Enum;
 
 namespace Test
 {
     public class CancelEventScreen : Screen
     {
+        private TopInfoComponent _topInfoComponent;
+
+        public override void OnLoading()
+        {
+            _topInfoComponent = new TopInfoComponent(this)
+            {
+                ArrowActive = false,
+                LeftButtonControl = new Image { Source = ResourceManager.GetImage("topheading_back") },
+                RightButtonControl = new TextView(Translator.Translate("send")),
+                Header = Translator.Translate("cancel"),
+                ArrowVisible = false
+            };
+        }
+
+        internal void TopInfo_LeftButton_OnClick(object sender, EventArgs args)
+        {
+            Navigation.Back();
+        }
+
+        internal void TopInfo_RightButton_OnClick(object sender, EventArgs args)
+        {
+            var eventId = (string)BusinessProcess.GlobalVariables[Parameters.IdCurrentEventId];
+            var @event = (Document.Event)DbRef.FromString(eventId).GetObject();
+            var commentMemoEdit = (MemoEdit)Variables["CommentaryMemoEdit"];
+            @event.Comment = commentMemoEdit.Text;
+            @event.Status = StatusyEvents.GetDbRefFromEnum(StatusyEventsEnum.Cancel);
+            DBHelper.SaveEntity(@event);
+            Navigation.CleanStack();
+            Navigation.ModalMove("EventListScreen");
+        }
+
+        internal void TopInfo_Arrow_OnClick(object sender, EventArgs args)
+        {
+            _topInfoComponent.Arrow_OnClick(sender, args);
+        }
+
+        internal string GetResourceImage(string tag)
+        {
+            return ResourceManager.GetImage(tag);
+        }
     }
 }
