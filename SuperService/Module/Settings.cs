e97810ccc4ba481034b2452d1c1d7dab04e2c805
Ilya Kuznetsov <x-i-ll@hotmail.com>
2016-07-18 13:47:42 +0300

    Добавил (пока что) все настройки в класс настроек

--- SuperService/Module/Settings.cs
+++ SuperService/Module/Settings.cs
@@ -6,14 +6,18 @@ namespace Test
     {
         public static string User { get; set; }
         public static string Password { get; set; }
-        public static string Server { get; set; }
+        public static string Server { get; private set; }
+        public static bool AllowGallery { get; private set; }
+        public static int PictureSize { get; private set; }
+        public static bool EquipmentEnabled { get; private set; }
+        public static bool CheckListEnabled { get; private set; }
+        public static bool COCEnabled { get; private set; }
+        public static bool BagEnabled { get; private set; }
+        public static bool ShowServicePrice { get; private set; }
+        public static bool ShowMaterialPrice { get; private set; }
 
         public static void Init()
         {
-            // TODO: Прописать нормальную инициализацию
-            User = "sr";
-            Password = "sr";
-            Server = "";
         }
     }
 }
