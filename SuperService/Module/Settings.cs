c13511f243f4b92c00198a04f767e136d0503b63
Ilya Kuznetsov <x-i-ll@hotmail.com>
2016-07-18 15:45:36 +0300

    Получаю пользователя и пароль из Context, поменял немного авторизацию

--- SuperService/Module/Settings.cs
+++ SuperService/Module/Settings.cs
@@ -1,10 +1,32 @@
-﻿namespace Test
+﻿using BitMobile.Application;
+
+namespace Test
 {
     public static class Settings
     {
-        public static string User { get; set; }
-        public static string Password { get; set; }
-        public static string Server { get; private set; }
+        public static string User
+        {
+            get { return ApplicationContext.Current.Settings.UserName; }
+            set
+            {
+                ApplicationContext.Current.Settings.UserName = value;
+                ApplicationContext.Current.Settings.WriteSettings();
+            }
+        }
+
+        public static string Password
+        {
+            get { return ApplicationContext.Current.Settings.Password; }
+            set
+            {
+                ApplicationContext.Current.Settings.Password = value;
+                ApplicationContext.Current.Settings.WriteSettings();
+            }
+        }
+
+        // TODO: Хранить сервер
+        public static string Server => ApplicationContext.Current.Settings.Url;
+
         public static bool AllowGallery { get; private set; }
         public static int PictureSize { get; private set; }
         public static bool EquipmentEnabled { get; private set; }
@@ -16,8 +38,7 @@
 
         public static void Init()
         {
-            Server = "";
-
+            ApplicationContext.Current.Settings.ReadSettings();
             var settings = DBHelper.GetSettings();
             AllowGallery = (bool)settings["AllowGalery"];
             PictureSize = (int)settings["PictureSize"];
