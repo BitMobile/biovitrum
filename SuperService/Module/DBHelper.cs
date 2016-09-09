9da439d76481e26e5244be2928aec8f9337a6262
Dmitrii <DPKuznechenkov@1cbit.ru>
2016-09-09 18:23:01 +0300

    Задача завершена. Больше нельзя изменить добавленные/назначенные услуги/материалы.

--- SuperService/Module/DBHelper.cs
+++ SuperService/Module/DBHelper.cs
@@ -179,7 +179,8 @@ namespace Test
                 DConsole.WriteLine(Parameters.Splitter);
                 DConsole.WriteLine($"Новые данные не пришли," +
                                    $"настройки не обновляем" +
-                                   $" {nameof(resultEventArgs.Result)} = {resultEventArgs.Result}");
+                                   $" {nameof(resultEventArgs.Result)} = {resultEventArgs.Result}" +
+                                   $"{Environment.NewLine}Last Error {_db.LastError}");
                 DConsole.WriteLine(Parameters.Splitter);
 #endif
                 return;
